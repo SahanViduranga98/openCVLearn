e7add5aec329e8876e643cefb0bb3fe6c173cfcd
SahanViduranga98 <sahanvidurangassc767@gmail.com>
2024-10-03 13:06:25 +0530

    color some regiion in a picture

--- draw.py
+++ draw.py
@@ -1,9 +1,13 @@
 import cv2 as cv
 import numpy as np
 
-blank =np.zeros((500,500),dtype='uint8')
+#create blank image
+blank =np.zeros((500,500,3),dtype='uint8')
+
+        #change the color od some spave, [height region,width region] 
+blank[0:166,0:250] = 0,0,255
+
+
 cv.imshow("blank",blank)
-
-
 cv.waitKey(0)
 #img = cv.imread('photos/brownCat.jpg')
