63a416d3eb1dcd5aedab5664c7cc428fa2d1a87d
SahanViduranga98 <sahanvidurangassc767@gmail.com>
2024-10-03 13:30:46 +0530

    Draw a line

--- draw.py
+++ draw.py
@@ -7,11 +7,21 @@ blank =np.zeros((500,500,3),dtype='uint8')
         #change the color od some region, [height region,width region] 
 #blank[0:166,0:250] = 0,0,255
 #draw a rectangle in an image
-#(image, start point, height & width, color of rectangle, thickness)
+#(image, mid point, height & width, color of rectangle, thickness)
 #cv.rectangle(blank,(0,0),(250,500),(0,255,0),thickness=2)
 
 #get the half of width and height  of image size rectangle
-cv.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(0,255,0),thickness=cv.FILLED)
+#cv.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(0,255,0),thickness=cv.FILLED)
+cv.imshow("blank",blank)
+
+#draw a circle
+#40 is the radius of the circle
+cv.circle(blank,(blank.shape[1]//2,blank.shape[0]//2),40,(0,0,255))
+
+
+#draw a line
+#line draws from 0,0 to 250,250
+cv.line(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(255,0,0),thickness=3)
 cv.imshow("blank",blank)
 cv.waitKey(0)
 #img = cv.imread('photos/brownCat.jpg')
