96e2d593509b37372608a395fb4add4e4966f77d
SahanViduranga98 <sahanvidurangassc767@gmail.com>
2024-10-03 15:16:48 +0530

    Blur an image

--- basic.py
+++ basic.py
@@ -5,4 +5,9 @@ cv.imshow('Cat',img) #show image in new window
 # gray scale image
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow('Cat_gary',gray) #show image in new window
+
+#blur an image
+#(7,7) is the kernel size, has to an odd number, increasing kernal number-->more blur
+blur = cv.GaussianBlur(img,(7,7),cv.BORDER_DEFAULT)
+cv.imshow('Blur',blur) #show image in new window
 cv.waitKey(0)
